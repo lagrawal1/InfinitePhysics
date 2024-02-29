5a0c9a81ae0bd254cb9434aa18c42982a1db5643
Loki <loku.cs.agrawal@gmail.com>
2024-02-29 13:42:37 +0530

    afasf

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
 
+function KinematicEq1(){
+    // v =u +at
+    let RNG = Math.floor(Math.random * 4);
+    if (RNG == 0){
+        //Solve for v
+        let u = Math.floor(Math.random * 100);
+        let a = Math.floor(Math.random * 100);
+        let t = Math.floor(Math.random * 100);
+        // Get innerhtml and create question here.
+
+        v = u +a*t;
+        var input = document.getElementById(); // Enter id of input
+        if (input == v){
+            alert("correct");
+        } else {
+            alert("nope");
+        }
+
+
+    }
+
+
+    
+} 
+
+function BasicKinematic(EqNum){
+    let n = Math.floor(Math.random()*10);
+    var s, u, v, a, t, input;
+    if (EqNum == 0){
+        // v = u +at
+        KinematicEq1()
+        
+
+    }
+
+}
+
+
