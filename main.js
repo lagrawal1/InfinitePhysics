e06420931dfc55999d776745833ef32e27240b0d
Loki <loku.cs.agrawal@gmail.com>
2024-02-29 16:07:31 +0530

    commit message

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var Reference = 0;
+
 function KinematicEq1(Val1 , Val2, Val3,Unknown, ParameterNames){
 
     Val1 = Math.floor(Math.random() * 20);
@@ -24,48 +26,41 @@ function KinematicEq1(Val1 , Val2, Val3,Unknown, ParameterNames){
 
 }
 
-
-
-
 function GenerateKinematic(){
     let EqNum = Math.floor(Math.random() *5);
     EqNum = 0;
-
-
     var s, u, v, a, t;
     if (EqNum == 0){
 
         var RNG = Math.floor(Math.random()*4);
 
         if (RNG == 0){
-            v = KinematicEq1(u , a, t, "v" , ["u" , "a", "t"]);
-            return v;
+            Reference = KinematicEq1(u , a, t, "v" , ["u" , "a", "t"]);
+            alert(Reference);
         } else if (RNG == 1){
-            u = KinematicEq1(v , a, t, "u" , ["v" , "a", "t"]);
-            return u;
-
+            window.Reference = KinematicEq1(v , a, t, "u" , ["v" , "a", "t"]);
+            alert(Reference);
         }else if (RNG == 2){
-            a = KinematicEq1(v,u,t,"a", ["v","u","t"])
-            return a;
-            
+            window.Reference = KinematicEq1(v,u,t,"a", ["v","u","t"]);
+            alert(Reference);
         }else if (RNG ==3){
             //Solve for a
-            t = KinematicEq1(v,u,a,"t", ["v","u","a"])
-            return t;
+            window.Reference  = KinematicEq1(v,u,a,"t", ["v","u","a"]);
+            alert(Reference);
+            
         }
     }
-
-
     event.preventDefault();
 }
 
 
-
-
-function AnswerCheck(Reference){
+function AnswerCheck(){
+    alert(Reference);
     var Input = document.getElementById("KinematicInput").value; // Enter id of input
-    alert(Input);
-    if (Input == Reference){
+
+    if (Input.trim() ==""){
+        alert("Please enter a value.");
+    } else if (Input == Reference){
         alert("correct");
         GenerateKinematic();
     } else{
